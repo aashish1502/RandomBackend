f999e0ecb699425979b02eeac0b4c3feffd51540
HighTide <aashishsharma.ajm@gmail.com>
2023-09-22 20:29:59 +0530

    Added simple route for index page

--- WebApplicationSimple/src/main/java/com/example/webapplicationsimple/HelloControllor.java
+++ WebApplicationSimple/src/main/java/com/example/webapplicationsimple/HelloControllor.java
@@ -1,3 +1,5 @@
+package com.example.webapplicationsimple;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- WebApplicationSimple/src/main/java/com/example/webapplicationsimple/WebApplicationSimpleApplication.java
+++ WebApplicationSimple/src/main/java/com/example/webapplicationsimple/WebApplicationSimpleApplication.java
@@ -1,13 +1,37 @@
 package com.example.webapplicationsimple;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class WebApplicationSimpleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplicationSimpleApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+
+		return  args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot");
+
+			String[] springBeans = ctx.getBeanDefinitionNames();
+			Arrays.sort(springBeans);
+			for(String speaingBean : springBeans) {
+
+				System.out.println(speaingBean);
+
+			}
+
+		};
+
 	}
 
 }
